3105a196cd86809f4ab5a3c199a548cf321bf67a
shirai1 <shirai1@macnoMacBook-Pro.local>
2019-10-30 16:20:34 +0900

    [add] movie pred script

---
+++ script/predict/add_label_for_movie.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+import subprocess
+
+
+def main():
+    target_movie_file = '/usr/local/wk/data/fortnite/movie/test/8_test.mp4'
+
+    result_list = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1,
+                   1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1,
+                   1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                   1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                   1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                   1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1,
+                   1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+                   1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+
+    ffmpeg_cmd = ' '.join(['/usr/local/Cellar/ffmpeg/4.2.1_1/bin/ffmpeg', '-i', target_movie_file, '-filter_complex "'])
+    draw_text_str = ''
+
+    for i, result in enumerate(result_list):
+        if result == 1:
+            if draw_text_str == '':
+                draw_text_str += "drawtext=:text='in battle':enable='between(t," + str(i * 5) + "," + str(i * 5 + 4) + ")'" + \
+                                 ":x=w-200:y=50:fontcolor=yellow:fontsize=64"
+            else:
+                draw_text_str += ",drawtext=:text='in battle':enable='between(t," + str(i * 5) + "," + str(i * 5 + 4) + ")'" +  \
+                                 ":x=w-200:y=50:fontcolor=yellow:fontsize=64"
+
+    print(ffmpeg_cmd)
+    print(draw_text_str)
+    print('" -acodec copy output.mp4')
+
+    # ffmpeg_cmd = ffmpeg_cmd + draw_text_str + '" -acodec copy output.mp4'
+
+    # print(ffmpeg_cmd.split())
+
+
+if __name__ == '__main__':
+    main()
